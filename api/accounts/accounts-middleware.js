01ced43c62a8097ed8b0a13f0faf8f917a004307
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-02-27 20:29:11 -0700

    created middlewares and db helpers

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,43 @@
+const Accounts = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  const { name, budget } = req.body;
+  if (name === undefined || budget === undefined) {
+    res.send(400).json({ message: "name and budget are required" })
+  }
+  if (name.length.trim() < 3 || name.length.trim() > 100 ) {
+    res.send(400).json({ message: "name of account must be between 3 and 100" })
+  } 
+  if (typeof budget != 'number') {
+    res.send(400).json({ message: "budget of account must be a number" })
+  }
+  if (budget < 0 || budget > 1000000) {
+    res.send(400).json({ message: "budget of account is too large or too small"})
+  }
+  next()
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
+  const account = await Accounts.getById(req.params.id)
+  if (!account) {
+    res.send(404).json({ message: "account not found" })
+  } else {
+    next()
+  }
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  db('accounts').where('name', req.body.name)
+  .then(acctName => {
+    if (acctName) {
+      next()
+    } else {
+      res.send(400).json({ message: "that name is taken" })
+    }
+  })
 }
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,53 @@
 const getAll = () => {
   // DO YOUR MAGIC
+  db('accounts')
+  .then(accounts => {
+    return accounts;
+  })
+  .catch(err => {
+
+  })
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  db('accounts').where('id', id).first()
+  .then(account => {
+    return account;
+  })
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  db('accounts').insert({ name: account.name, budget: account.budget })
+  .then(newId => {
+    return getById(newId)
+  })
+  .then(newAcc => {
+    return newAcc;
+  })
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  db('accounts').where('id', id).update({ ...account, name: account.name, budget: account.budget })
+  .then(records => {
+    return getById(id)
+  })
+  .then(updated => {
+    return updated;
+  })
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  db('accounts').where('id', id).delete()
+  .then(num => {
+    return getById(id)
+  })
+  .then(deleted => {
+    return deleted;
+  })
 }
 
 module.exports = {
